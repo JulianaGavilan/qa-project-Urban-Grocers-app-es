e2cb1d04ff0f09745ae98a7eb998c67b9bc79af4
JulianaGavilan <juliana.gavilanp98@gmail.com>
2024-12-29 15:05:22 -0500

    commit sprint7

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-1ec7fc31-198d-44e5-a6be-e7394e25636d.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-c2d1eb80-0482-4963-aeed-cd0bee80642c.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_NEW_KIT = "/api/v1/kits"
